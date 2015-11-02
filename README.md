f550ba11ed99c0389cc0a1e832daa02986dd0ef7
rongself <rongself1991@gmail.com>
2015-11-02 15:24:14 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+## 简介
+这个一个微信AccessToken中控服务器,用于统一管理刷新AccessToken,本程序使用Redis来储存AccessToken
+
+## 安装
+
+```bash
+$ npm install wxatm
+```
+## 配置
+
+```bash
+$ cp ./config.json.default config.json #复制一份配置文件
+```
+
+```js
+{
+  "wxAppId": "xxxxxxxxxxxxxxxxxxx",   //微信AppId
+  "wxAppSecret": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", //微信AppSecret
+  "store":"memory", //储存方式,有效值为memory|redis ,设置为memory时,会将accessToken存储在变量中
+  "port": 4730, //HTTP端口
+  "https":{
+      "enable":true, 是否使用HTTPS
+      "port":4740,  //HTTPS端口
+      "sslCert": "./sslcert/server.crt",
+      "sslKey": "./sslcert/server.key"
+    },
+  "auth":true,    //是否使用HTTP Base Authorization
+  "apps": [       //HTTP Base Authorization的用户名和密码,当auth为false此参数忽略
+    {
+      "appId": "UNIKEY1",
+      "appSecret": "SECRET"
+    },
+    {
+      "appId": "UNIKEY2",
+      "appSecret": "SECRET"
+    },
+    {
+      "appId": "UNIKEY3",
+      "appSecret": "SECRET"
+    }
+  ]
+}
+```
+
+## 使用
+
+建议选择你喜欢的进程管理器来运行,如forever或pm2
+```bash
+$ pm2 ./index.js
+```
+程序启动后就可以通过Restful请求来获取AccessToken,如对安全性有要求,建议使用HTTPS + HTTP Base Authorization
+
+可以用curl命令来测试是否成功
+```bash
+$ curl -X GET -v --user 'UNIKEY:SECRET' http://vm.local:4730/access-token.json #获取accessToken
+$ curl -X POST -v --user 'UNIKEY:SECRET' http://vm.local:4730/refresh #手动刷新accessToken
+```
+
+## License
+
+  [MIT](LICENSE)
+
---
+++ index.js
@@ -0,0 +1,88 @@
+#!/usr/bin/env node
+"use strict"
+var http = require('http');
+var express = require('express');
+var AccessToken = require('./lib/accessToken');
+var config = require('./config.json');
+var storeFactory = require('./lib/store/factory');
+
+var app = express();
+var wxAppId = config.wxAppId;
+var wxAppSecret = config.wxAppSecret;
+var apps = config.apps;
+var token = new AccessToken(wxAppId,wxAppSecret,storeFactory.get(config.store || 'memory'));
+
+token.refreshToken();
+var Auth = {
+    isValid: function (req) {
+
+        if (!req.headers.authorization) return false;
+
+        var encoded = req.headers.authorization.split(' ')[1];
+        var decoded = new Buffer(encoded, 'base64').toString('utf8');
+        var id = decoded.split(':')[0];
+        var secret = decoded.split(':')[1];
+
+        for (var i = 0; i < apps.length; i++) {
+            if(id === apps[i].appId && secret === apps[i].appSecret){
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+
+}
+
+
+app.get('/access-token.json', function (req,res) {
+
+    if (config.auth !==false && !Auth.isValid(req)) {
+        return res.sendStatus(401);
+    }
+
+    token.getToken().then(function (data) {
+        res.json({accessToken:data});
+    }, function (err) {
+        res.sendStatus(504);
+        console.log(err);
+    });
+
+});
+
+app.post('/refresh', function (req,res) {
+
+    if (config.auth !==false && !Auth.isValid(req)) {
+        return res.sendStatus(401);
+    }
+
+    token.refreshToken().then(
+        function (data) {
+            res.sendStatus(200);
+        },
+        function (err) {
+            res.sendStatus(504);
+            console.log(err);
+        }
+    );
+
+});
+
+var httpServer = http.createServer(app);
+httpServer.listen(config.port || 80);
+console.log('Http server started,port:'+ (config.port || 80));
+
+//Handle https
+if(config.https && config.https.enable === true){
+    var fs = require('fs');
+    var https = require('https');
+
+    var privateKey  = fs.readFileSync(config.https.sslKey, 'utf8');
+    var certificate = fs.readFileSync(config.https.sslCert, 'utf8');
+    var credentials = {key: privateKey, cert: certificate};
+
+    var httpsServer = https.createServer(credentials, app);
+    httpsServer.listen(config.https.port || 443);
+    console.log('Https server started,port:'+ (config.https.port || 443));
+}
